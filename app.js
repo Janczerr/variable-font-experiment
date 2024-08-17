b3d4a9f6fd2d00f0a68badcf9c742f31c4c6de26
Janczerr <lukasz.jankowwski@gmail.com>
2024-08-17 10:02:03 +0200

    v1 working

--- app.js
+++ app.js
@@ -13,8 +13,30 @@ while (i < logoLength){
 
 logo.innerHTML = innerSpans;
 
-let spans = document.getElementsByClassName("weight");
+let spans = Array.from(document.getElementsByClassName("weight"));
 
-Array.from(spans).forEach(element => {
-    console.log(element);
+spans.forEach((span, index) => {
+    span.addEventListener(
+        "mouseover",
+        () => {
+            spans[index - 1].className = spans[index - 1].className + " left-1";
+            spans[index - 2].className = spans[index - 2].className + " left-2";
+
+            spans[index + 1].className = spans[index - 1].className + " right-1";
+            spans[index + 2].className = spans[index - 2].className + " right-2";
+         
+        }
+    );
+
+    span.addEventListener(
+        "mouseleave",
+        () => {
+            spans[index - 1].className = "weight";
+            spans[index + 1].className = "weight";
+            spans[index - 2].className = "weight";
+            spans[index + 2].className = "weight";
+        }
+    )
 });
+
+console.log(spans[54].className);
